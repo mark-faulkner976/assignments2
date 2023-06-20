176d7b30c016b8f2d3f60cf6695fd7ccc505a4be
Mark Faulkner <mark.faulkner976@gmail.com>
2023-06-20 16:34:35 -0500

    setting up the local storage

--- dev-lvl-6/rlm-token-auth-series-context-pt2/client/src/components/Navbar.js
+++ dev-lvl-6/rlm-token-auth-series-context-pt2/client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar(){
   return (
-    <div className="navbar">
+    <div className="navbar-wrapper">
       <Link to="/profile">Profile</Link>
       <Link to="/public">Public</Link>
     </div>
--- dev-lvl-6/rlm-token-auth-series-context-pt2/client/src/index.js
+++ dev-lvl-6/rlm-token-auth-series-context-pt2/client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import "./index.css";
+import UserProvider from './context/UserProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
