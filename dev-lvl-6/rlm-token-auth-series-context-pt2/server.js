74fa3aebbbf9cdce0842cb5576b691ce32f508b7
Mark Faulkner <mark.faulkner976@gmail.com>
2023-06-20 15:03:41 -0500

    setting up server for part 2

--- dev-lvl-6/rlm-token-auth-series-context-pt2/server.js
+++ dev-lvl-6/rlm-token-auth-series-context-pt2/server.js
@@ -9,8 +9,8 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 mongoose.connect(
-  'mongodb://localhost:27017/user-authentication',
-  () => console.log('Connected to the DB')
+  `${process.env.MONGO_URL}`,
+  () => console.log("Connected to the DB")
 )
 
 app.use('/auth', require('./routes/authRouter.js'))
@@ -25,6 +25,6 @@ app.use((err, req, res, next) => {
   return res.send({errMsg: err.message})
 })
 
-app.listen(9000, () => {
-  console.log(`Server is running on local port 9000`)
-})
+app.listen( process.env.PORT, () => {
+  console.log(`Server is running on local port ${process.env.PORT}`)
+} )
